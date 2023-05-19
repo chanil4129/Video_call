89d690bc98b7d52a0b04cec0a045b8adbf6d6f01
chanil <chanil4129@naver.com>
2023-05-19 11:36:02 +0900

    mouse event add(drag, right click)

--- remote_client.py
+++ remote_client.py
@@ -6,19 +6,7 @@ from PIL import Image, ImageTk
 import websockets
 import asyncio
 
-ADDR="ws://192.1.1.1:8765" # 주소 및 포트
-
-# 서버에게 명령을 보내는 함수
-async def send_commands():
-    async with websockets.connect(ADDR) as websocket:
-        # 마우스 이동 명령 전송
-        await websocket.send(json.dumps({"type": "move_to", "x": 100, "y": 100}))
-
-        # 마우스 클릭 명령 전송
-        await websocket.send(json.dumps({"type": "click", "x": 100, "y": 100, "button": "left"}))
-
-        # 키보드 입력 명령 전송
-        await websocket.send(json.dumps({"type": "write"}))
+ADDR="ws://192.1.1.1:8765"
 
 # 서버에게 스크린샷을 요청하는 함수
 async def request_screenshot(websocket):
@@ -42,9 +30,31 @@ async def update_screen(websocket, canvas):
 async def main(websocket, root, canvas):
     loop = asyncio.get_event_loop()
 
-    # 마우스 클릭 이벤트 처리 함수
-    def on_mouse_click(event):
-        data = {"type": "mouse_click", "data": [event.x, event.y, "left"]}
+    start = None
+
+    # 드래그 시작 이벤트 처리 함수
+    def on_mouse_down(event):
+        nonlocal start
+        start = (event.x, event.y)
+        data = {"type": "mouse_down", "data": [start[0], start[1], "left"]}
+        loop.call_soon_threadsafe(lambda: asyncio.create_task(websocket.send(json.dumps(data))))
+
+    # 드래그 종료 이벤트 처리 함수
+    def on_mouse_up(event):
+        nonlocal start
+        if start is not None:
+            data = {"type": "mouse_up", "data": [event.x, event.y, "left"]}
+            loop.call_soon_threadsafe(lambda: asyncio.create_task(websocket.send(json.dumps(data))))
+            start = None
+
+    # 오른쪽 마우스 버튼 누름 이벤트 처리 함수
+    def on_right_mouse_down(event):
+        data = {"type": "mouse_down", "data": [event.x, event.y, "right"]}
+        loop.call_soon_threadsafe(lambda: asyncio.create_task(websocket.send(json.dumps(data))))
+
+    # 오른쪽 마우스 버튼 놓음 이벤트 처리 함수
+    def on_right_mouse_up(event):
+        data = {"type": "mouse_up", "data": [event.x, event.y, "right"]}
         loop.call_soon_threadsafe(lambda: asyncio.create_task(websocket.send(json.dumps(data))))
 
     # 키보드 입력 이벤트 처리 함수
@@ -52,7 +62,10 @@ async def main(websocket, root, canvas):
         data = {"type": "write", "text": event.char}
         loop.call_soon_threadsafe(lambda: asyncio.create_task(websocket.send(json.dumps(data))))
 
-    canvas.bind("<Button-1>", on_mouse_click)  # 캔버스에 마우스 클릭 이벤트 바인딩
+    canvas.bind("<Button-1>", on_mouse_down)  # 캔버스에 마우스 버튼 누름 이벤트 바인딩
+    canvas.bind("<ButtonRelease-1>", on_mouse_up)  # 캔버스에 마우스 버튼 놓음 이벤트 바인딩
+    canvas.bind("<Button-3>", on_right_mouse_down)  # 캔버스에 오른쪽 마우스 버튼 누름 이벤트 바인딩
+    canvas.bind("<ButtonRelease-3>", on_right_mouse_up)  # 캔버스에 오른쪽 마우스 버튼 놓음 이벤트 바인딩
     root.bind("<Key>", on_key_press)  # 루트 윈도우에 키보드 이벤트 바인딩
 
     while True:
@@ -67,15 +80,12 @@ async def run_main(root, canvas):
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 root = tk.Tk()  # 루트 윈도우 생성
-canvas = tk.Canvas(root, width=800, height=600)  # 캔버스 생성
+canvas = tk.Canvas(root, width=1440, height=900)  # 캔버스 생성
 canvas.pack()
 
-# 별도의 스레드에서 tkinter 이벤트 루프 실행
+# 별도의 스레드에서 tkinter 이벤트 루프
 import threading
 threading.Thread(target=asyncio.run, args=(run_main(root, canvas),), daemon=True).start()
 
 # tkinter 메인 루프 시작
 root.mainloop()
-
-# 서버에게 명령 보내기
-asyncio.run(send_commands())
--- remote_server.py
+++ remote_server.py
@@ -5,52 +5,51 @@ import json
 import base64
 import io
 
-# 클라이언트로부터 받은 메시지를 처리하는 함수
-async def remote_control(websocket, path):
+# 클라이언트와의 연결을 관리하는 함수
+async def handle_client(websocket, path):
     async for message in websocket:
         data = json.loads(message)
 
-        # 마우스 이동
-        if data["type"] == "move_to":
-            pyautogui.moveTo(data["x"], data["y"])
+        # 화면 스크린샷 요청 처리
+        if data["type"] == "request_screen":
+            screenshot = pyautogui.screenshot()
+            buffered = io.BytesIO()
+            screenshot.save(buffered, format="PNG")
+            screenshot_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+            # 클라이언트에게 스크린샷 전송
+            await websocket.send(json.dumps({"type": "screen", "data": screenshot_base64}))
 
-        # 마우스 클릭
-        elif data["type"] == "click":
-            pyautogui.click(data["x"], data["y"], button=data["button"])
+        # 마우스 클릭 요청 처리
+        elif data["type"] in ["mouse_down", "mouse_up"]:
+            x, y, button = data["data"]
+            if data["type"] == "mouse_down":
+                pyautogui.mouseDown(x, y, button=button)
+            else:
+                pyautogui.mouseUp(x, y, button=button)
 
-        # 키보드 입력
+        # 키보드 입력 요청 처리
         elif data["type"] == "write":
-            pyautogui.write(data["text"])
+            text = data["text"]
+            pyautogui.write(text)
 
-# 클라이언트와의 연결을 관리하는 함수
-async def handle_client(websocket, path):
-    while True:
-        try:
-            # 클라이언트로부터 메시지 받기
-            message = await websocket.recv()
-            data = json.loads(message)
 
-            # 화면 스크린샷 요청 처리
-            if data["type"] == "request_screen":
-                screenshot = pyautogui.screenshot()
-                buffered = io.BytesIO()
-                screenshot.save(buffered, format="PNG")
-                screenshot_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
-                # 클라이언트에게 스크린샷 전송
-                await websocket.send(json.dumps({"type": "screen", "data": screenshot_base64}))
+        # 마우스 드래그
+        elif data["type"] == "drag":
+            x1, y1, x2, y2, button = data["data"]
+            pyautogui.moveTo(x1, y1)
+            pyautogui.dragTo(x2, y2, button=button)
 
-            # 마우스 클릭 요청 처리
-            elif data["type"] == "mouse_click":
-                x, y, button = data["data"]
-                pyautogui.click(x, y, button=button)
+        # 마우스 누르기
+        elif data["type"] == "mouse_down":
+            x, y, button = data["data"]
+            pyautogui.mouseDown(x, y, button=button)
 
-            # 키보드 입력 요청 처리
-            elif data["type"] == "write":
-                text = data["text"]
-                pyautogui.write(text)
+        # 마우스 떼기
+        elif data["type"] == "mouse_up":
+            x, y, button = data["data"]
+            pyautogui.mouseUp(x, y, button=button)
+        
 
-        except websockets.ConnectionClosed:
-            break
 
 # 웹소켓 서버 시작
 start_server = websockets.serve(handle_client, "0.0.0.0", 8765)
